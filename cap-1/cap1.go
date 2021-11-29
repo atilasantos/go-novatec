a7c817fc3ec8bc6d85da8a053c6385a40efc7d07
Atila Santos <atila.romao@hotmail.com>
2021-11-29 14:32:36 -0300

    cap1 - implementado com serviço web

--- cap-1/cap1.go
+++ cap-1/cap1.go
@@ -1,5 +1,19 @@
 package cap1
 
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+)
+
+func GetNumbers(c *gin.Context) {
+	n1, _ := strconv.Atoi(c.Query("number1"))
+	n2, _ := strconv.Atoi(c.Query("number2"))
+
+	c.IndentedJSON(http.StatusOK, n1+n2)
+}
+
 func Sum(n1 int, n2 int) int {
 	return n1 + n2
 }
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
-
 	cap1 "github.com/atilasantos/go-novatec/cap-1"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	n1, _ := strconv.Atoi(os.Args[1])
-	n2, _ := strconv.Atoi(os.Args[2])
+	// n1, _ := strconv.Atoi(os.Args[1])
+	// n2, _ := strconv.Atoi(os.Args[2])
 
-	fmt.Println(cap1.Sum(n1, n2))
+	// fmt.Println(cap1.Sum(n1, n2))
+
+	router := gin.Default()
+	router.GET("/sum", cap1.GetNumbers)
+	router.Run("localhost:8000")
 }
